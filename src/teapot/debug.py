4858a02480ee4e520228ad01b389633f3dcf40df
Simarpreet Singh <simarsaini13@gmail.com>
2026-08-19 18:35:54 +0100

    Found issue, build dir not available in checking, fixed.

--- src/teapot/debug.py
+++ src/teapot/debug.py
@@ -1,8 +1,11 @@
 import builtins
+from pathlib import Path
 
 
 def print(*args, **kwargs):
     text = " ".join(str(arg) for arg in args)
+
+    Path("build").mkdir(parents=True, exist_ok=True)
 
     with open("build/build.log", "at") as file:
         file.write(text + "\n")
